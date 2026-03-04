a504bc82fcb72958222d2ef4c22d8c1018f6ad5c
thadio <thadio_robledo@hotmail.com>
2026-03-04 14:08:42 -0300

    reversao de backup

--- _recovery/20260304-135153/app.thadio.com/marca-quick-create.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__ . '/bootstrap.php';
-
-use App\Controllers\BrandController;
-
-[$pdo, $connectionError] = bootstrapPdo();
-$controller = new BrandController($pdo, $connectionError);
-$controller->quickCreate();
